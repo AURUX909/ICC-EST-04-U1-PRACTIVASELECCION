96948b49b63b057b5cf173bb1b52e47017dcae69
Renato <ayadaicela@est.ups.edu.ec>
2024-09-27 12:46:01 -0500

    Finalizacion del programa Seleccion

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Métodos de ordenamiento selección");
         Seleccion sel = new Seleccion();
         int[] arreglo = {2, 3, 4, 5, 19, 0};
-        sel.selectionSort(arreglo);
+
+        // Ordenar en orden ascendente
+        sel.selectionSort(arreglo, true);
+        System.out.print("Arreglo ordenado (ascendente): ");
+        sel.printArreglo(arreglo); // Imprimir el arreglo ordenado
+
+        // Ordenar en orden descendente
+        sel.selectionSort(arreglo, false);
+        System.out.print("Arreglo ordenado (descendente): ");
         sel.printArreglo(arreglo); // Imprimir el arreglo ordenado
     }
 }
+
--- src/Seleccion.java
+++ src/Seleccion.java
@@ -1,19 +1,23 @@
 public class Seleccion {
-    public int[] selectionSort(int[] arreglo) {
+    public int[] selectionSort(int[] arreglo, boolean ascendente) {
         int tamanio = arreglo.length;
-
         // Recorrer el arreglo
         for (int i = 0; i < tamanio; i++) {
             int indice = i;
-            for (int j = i + 1; j < tamanio; j++) {
-                if (arreglo[j] < arreglo[indice]) { // Cambiar a menor para orden ascendente
+            for (int j = i + 1; j < tamanio; j++) { 
+                // Comparar según el orden deseado
+                if (ascendente 
+                ? arreglo[j] < arreglo[indice] 
+                : arreglo[j] > arreglo[indice]) {
                     indice = j;
                 }
             }
             // Intercambio del índice encontrado por el primero
-            int aux = arreglo[indice];
-            arreglo[indice] = arreglo[i];
-            arreglo[i] = aux;
+            if (indice != i) { // Solo intercambiar si es necesario
+                int aux = arreglo[indice];
+                arreglo[indice] = arreglo[i];
+                arreglo[i] = aux;
+            }
         }
         return arreglo;
     }
